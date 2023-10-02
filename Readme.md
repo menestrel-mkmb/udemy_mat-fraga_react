92f2d059eec0de7f2e9427e12f2dba9809d346ab
Michael <michaelkmb@hotmail.com>
2023-10-02 10:39:08 -0300

    + dom manipulation for replace n append nodes;

--- Readme.md
+++ Readme.md
@@ -3,4 +3,5 @@
 Este repositório tem como objetivo relembrar os conceitos do React.js
 
 [Link do Curso da Udemy](https://www.udemy.com/course/curso-reactjs/)
+
 [Documentação React](https://react.dev/)
--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const input = prompt("Digite algo no prompt");
-console.log(input);
-
 const content = document.querySelector(".content");
-const text = document.createElement("p");
-content.appendChild(text);
-text.textContent = input;
+const loginBtn = document.querySelector(".loginBtn");
+
+loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    loginBtn.textContent = "Sair";
+    const text = document.createElement("p");
+    text.textContent = "Você está logado ";
+    loginBtn.replaceWith(text);
+    content.appendChild(loginBtn);
+});
