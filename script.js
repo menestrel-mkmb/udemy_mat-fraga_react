0d1eb92618758a34982368d5ca35c2a57ce009fd
Michael <michaelkmb@hotmail.com>
2023-10-02 19:24:55 -0300

    + localStorage login/logout component;

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
 const content = document.querySelector(".content");
 const loginBtn = document.querySelector(".loginBtn");
 
+const createOtherUserLogin = () => {
+    const otherAcc = document.createElement("p");
+    otherAcc.textContent = "Não é você? Trocar usuário";
+    otherAcc.className ="otherUserText";
+    otherAcc.addEventListener("click", (e) => {
+        e.preventDefault();
+        if(localStorage.getItem("nome")) localStorage.removeItem("nome");
+        logout();
+    })
+    content.appendChild(otherAcc);
+}
+
+const createYouAreLoggedIn = () => {
+    const loginTxt = document.createElement("p");
+    loginTxt.textContent = localStorage.nome + ", você está logado.";
+    loginTxt.className = "youAreLoggedText";
+    content.appendChild(loginTxt);
+}
+
+const login = () => {
+    while(typeof localStorage.nome == 'undefined') localStorage.nome = prompt("Qual seu nome?");
+    
+    createYouAreLoggedIn();
+    createOtherUserLogin();
+    
+    loginBtn.textContent = "Sair";
+}
+
+const logout = () => {
+    loginBtn.textContent = "Entrar";
+    const loginTxt = document.querySelector(".youAreLoggedText");
+    loginTxt.parentNode.removeChild(loginTxt);
+    const otherUserTxt = document.querySelector(".otherUserText");
+    otherUserTxt.parentNode.removeChild(otherUserTxt);
+}
+
 loginBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    const text = document.querySelector(".loggedText");
-    if(loginBtn.textContent == "Entrar") {
-        loginBtn.textContent = "Sair";
-        const text = document.createElement("p");
-        text.textContent = "Você está logado.";
-        text.className = "loggedText";
-        loginBtn.replaceWith(text);
-        content.appendChild(loginBtn);
-    } else {
-        loginBtn.textContent = "Entrar";
-        text.parentNode.removeChild(text);
-    }
+    (loginBtn.textContent == "Entrar") ?
+        login() :
+        logout();
 });
