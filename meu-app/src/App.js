86ae156c7df6c20c33c89304d83b5fd3f241813c
Michael <michaelkmb@hotmail.com>
2023-10-03 18:16:58 -0300

    stateless component n props;

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+const BemVindo = (props) => <h1>Bem-vindo(a) {props.nome} ao curso de React</h1>
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<>
+    <BemVindo nome="Michael"/>
+    <BemVindo nome="Fulano"/>
+    </>);
 }
 
 export default App;
