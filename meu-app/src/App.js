59d35c861d6fb6b263e65a51ab8c9b44923daf06
Michael <michaelkmb@hotmail.com>
2023-10-04 19:27:33 -0300

    + Component lifecycle;

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -5,9 +5,32 @@ import Person from './components/Person/Person';
 import Equipe from './components/Equipe/Equipe';
 import { Component } from 'react';
 
+class Relogio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hora: '00:00:00'
+    };
+  }
+
+  componentDidMount(){
+    setInterval(() => {
+      this.setState({ hora: new Date().toLocaleTimeString()});
+    }, 1000);
+  }
+
+  render() {
+    return(<>
+    <p>Hora atual: {this.state.hora}</p>
+    </>);
+  }
+}
+
 function App() {
   return (<>
     <BemVindo nome="Fulano"/>
+
+    <Relogio/>
 
     <Equipe/>
     <Person name="Michael" job="developer" />
