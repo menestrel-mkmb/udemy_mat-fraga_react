80647eb34f621e39ce67f2dc175635cf86992de6
Michael <michaelkmb@hotmail.com>
2023-10-04 16:18:11 -0300

    + state component;

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -2,12 +2,55 @@ import logo from './logo.svg';
 import './App.css';
 import BemVindo from './components/BemVindo/BemVindo';
 import Person from './components/Person/Person';
+import { Component } from 'react';
+
+class Equipe extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      chamada: "Bem-vindo ao nosso site",
+      contador: 0
+    }
+
+    this.aumentar = this.aumentar.bind(this);
+    this.diminuir = this.diminuir.bind(this);
+  }
+
+  aumentar() {
+    let state = this.state;
+
+    state.contador += 1;
+    if(state.contador >= 5) state.chamada = "Conheça nossa equipe";
+    
+    this.setState(state);
+  }
+  
+  diminuir() {
+    let state = this.state;
+
+    state.contador -= 1;
+    if(state.contador < 5) state.chamada = "Bem-vindo ao nosso site";
+
+    this.setState(state);
+  }
+
+  render() {
+    return(<>
+      <h3>{this.state.chamada}</h3>
+      <button onClick={this.diminuir}>-</button>
+      {this.state.contador}
+      <button onClick={this.aumentar}>+</button>
+    </>);
+  }
+}
 
 function App() {
   return (<>
     <BemVindo nome="Fulano"/>
 
-    <h2>Conheça nossa equipe</h2>
+    <Equipe/>
     <Person name="Michael" job="developer" />
     <Person name="Cicrano" job="qa" />
     <Person name="Beltrano" job="tester" />
