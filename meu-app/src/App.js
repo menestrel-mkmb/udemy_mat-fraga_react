b36ef80c5df6262fbb61cfff1d44c9af83ba1bb4
Michael <michaelkmb@hotmail.com>
2023-10-10 19:22:18 -0300

    __misc: show everything;

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -1,19 +1,18 @@
 import './App.css';
+import BemVindo from './components/BemVindo/BemVindo';
+import Relogio from './components/Relogio/Relogio';
+import Formulario from './components/Formulario/Formulario';
+import Equipe from './components/Equipe/Equipe';
+import FortuneCookie from './components/FortuneCookie/FortuneCookie';
 import Cronometro from './components/Cronometro/Cronometro';
-
-// import BemVindo from './components/BemVindo/BemVindo';
-// import Relogio from './components/Relogio/Relogio';
-// import Formulario from './components/Formulario/Formulario';
-// import Equipe from './components/Equipe/Equipe';
-// import FortuneCookie from './components/FortuneCookie/FortuneCookie';
 
 function App() {
   return (<>
-    {/* <BemVindo name="Fulano"/>
+    <BemVindo name="Fulano"/>
     <Relogio/>
+    <FortuneCookie />
     <Formulario />
     <Equipe/>
-    <FortuneCookie /> */}
     <Cronometro />
     </>);
 }
