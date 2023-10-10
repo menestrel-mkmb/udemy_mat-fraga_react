66a2a1121d2ed640588b4df861d0a060f482a741
Michael <michaelkmb@hotmail.com>
2023-10-10 19:23:54 -0300

    __fix: display flex on Person Component;

--- meu-app/src/components/Person/Person.js
+++ meu-app/src/components/Person/Person.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function Person(props) {
     return (
-    <>
+    <section className={"person__sect"}>
         {/* <img src="" alt="" /> */}
         <h4>{`[${props.id}] ${props.name}:`}</h4>
         <h3 className="person__subtitle subtitle">Olá, sou o(a) {props.name}</h3>
         <p>Trabalho na área de {props.job}</p>
         <p>{`${props.likes} curtida${props.likes > 1 ? "s ": " "}|
         ${props.comments} comentário${props.comments > 1 ? "s ": " "}`}</p>
-    </>);
+    </section>);
 }
 
 export default Person;
