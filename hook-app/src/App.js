6cd11c37fb5a85f34d09c16f9c862a436e21a7a3
Michael <michaelkmb@hotmail.com>
2023-10-11 11:19:07 -0300

    feat ToDo list CRUD;

--- hook-app/src/App.js
+++ hook-app/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [title, setTitle] = useState('Tarefas');
+  const [input, setInput] = useState('Adicione aqui sua tarefa');
+  const [addTaskText, setAddTaskText] = useState('Adicionar Tarefa');
+  const [tasks, setTasks] = useState([
+        'Pagar a conta de luz',
+        'Estudar React Hooks'
+  ]);
+
+  function addTask(e) {
+    e.preventDefault();
+    if(input === '') return;
+    setTasks([...tasks, input]);
+    setInput('');
+  }
+
+  function deleteTask(index) {
+    setTasks( 
+      tasks.filter( (task, taskIndex) => taskIndex !== index)
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testando
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>{title}</h2>
+      <input className={"newTask__inp"} type="text" value={input} onChange={e => setInput(e.target.value)}/>
+      <button onClick={addTask}>{addTaskText}</button>
+      <ul>
+        {tasks.map( (task, index) => {
+          return (
+          <section>
+            <li>{task}</li><button onClick={()=>{deleteTask(index)}}>X</button>
+          </section>);
+        }
+        )}
+      </ul>
     </div>
   );
 }
