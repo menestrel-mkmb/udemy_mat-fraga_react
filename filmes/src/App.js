91521318553ef6b822467003df65676848f3995a
Michael <michaelkmb@hotmail.com>
2023-10-20 18:13:05 -0300

    feat JSON consumption News;

--- filmes/src/App.js
+++ filmes/src/App.js
@@ -1,10 +1,31 @@
+import React from 'react';
+
+import obj from './data.json';
+
+import './style.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hello World</p>
+      <header className="header header__sect">
+        <h1 className={"site__title"}>ReactNutri</h1>
       </header>
+      <main>
+        {obj.map((post) => {
+          return(
+          <section key={post.id} className={"post__sect"}>
+            <img src={post.capa} alt="" />
+            <section>
+              <div className={"post__title-bar"}>
+                <h2>{post.titulo}</h2>
+                <h4>{post.categoria} ({post.id})</h4>
+              </div>
+              <h3>{post.subtitulo}</h3>
+            </section>
+          </section>);
+        })}
+      </main>
     </div>
   );
 }
