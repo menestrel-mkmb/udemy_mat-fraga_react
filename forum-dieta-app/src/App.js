6855196fe89c5e157325a410423f0aca00144816
Michael <michaelkmb@hotmail.com>
2023-10-27 15:30:12 -0300

    feat reuse Api for Post obj;

--- forum-dieta-app/src/App.js
+++ forum-dieta-app/src/App.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import "./style.css";
 
 import Post from "./components/Post/Post";
 import Header from "./components/Header/Header";
 
-import obj from "./data.json";
+import staticObj from "./data.json";
 
 function App() {
+  const [obj, setObj] = useState([]);
+
+  const fetchApi = useCallback(() => {
+    const baseURL = "https://sujeitoprogramador.com/rn-api/?api=posts";
+    fetch(baseURL)
+      .then((r) => r.json())
+      .then((json) => setObj(json));
+  }, []);
+
+  useEffect(fetchApi, [obj]);
+
+  obj === undefined && setObj(staticObj);
+
   return (
     <div className="App">
       <Header title="ReactNutri" />
@@ -14,6 +27,7 @@ function App() {
         {obj.map((post) => {
           return (
             <Post
+              key={post.id}
               titulo={post.titulo}
               categoria={post.categoria}
               capa={post.capa}
