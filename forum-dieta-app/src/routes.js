21f3a794fa1b046d2725e3aaa0364b6fdc41fda1
Michael <michaelkmb@hotmail.com>
2023-10-30 16:54:01 -0300

    fix path on Route About Component;

--- forum-dieta-app/src/routes.js
+++ forum-dieta-app/src/routes.js
@@ -9,7 +9,7 @@ function RoutesApp() {
     return(<BrowserRouter>
         <Routes>
             <Route path="/" element={ <App /> } />
-            <Route parth="/about" element={ <About /> } />
+            <Route path="/about" element={ <About /> } />
             <Route path="/contact" element={ <Contact /> } />
             <Route path="*" element={ <Error404 /> } />
         </Routes>
