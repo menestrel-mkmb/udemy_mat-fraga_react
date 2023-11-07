62a9426af040fab770ceff2cc1d53aa3c55179a1
Michael Kevin <michaelkmb@hotmail.com>
2023-11-06 23:49:07 -0300

    feat Header Component;

---
+++ minstrelflix/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+export default function Header() {
+    return(
+    <header className="App-header">
+        <p>
+        Header
+        </p>
+        <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        Learn React
+        </a>
+    </header>);
+}
