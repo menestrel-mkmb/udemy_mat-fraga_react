bee468ab5337bec66086810528568d7989d11b85
Michael Kevin <michaelkmb@hotmail.com>
2023-11-07 11:13:21 -0300

    misc routes for all Pages;

--- minstrelflix/src/routes.js
+++ minstrelflix/src/routes.js
@@ -3,14 +3,19 @@ import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header/Header";
 
 import Home from "./pages/Home/Home";
-// import Error404 from "./pages/Error404/Error404";
-// import Favorites from "./pages/Favorites/Favorites";
+import Error404 from "./pages/Error404/Error404";
+import Favorites from "./pages/Favorites/Favorites";
+import Movie from './pages/Movie/Movie';
 
 function RoutesApp() {
     return(<BrowserRouter>
         <Header />
         <Routes>
             <Route path="/" element={ <Home /> } />
+            <Route path="/favorites" element={ <Favorites /> } />
+            <Route path="/movie/:movieId" element={ <Movie /> } />
+
+            <Route path="/*" element={ <Error404 /> } />
         </Routes>
     </BrowserRouter>);
 }
