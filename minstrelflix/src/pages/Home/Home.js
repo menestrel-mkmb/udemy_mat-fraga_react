a64748783c3d7961f32093e83fae815694a63071
Michael Kevin <michaelkmb@hotmail.com>
2023-11-08 19:33:44 -0300

    feat fetch tmdb api;

--- minstrelflix/src/pages/Home/Home.js
+++ minstrelflix/src/pages/Home/Home.js
@@ -1,6 +1,33 @@
+import { useEffect, useState } from "react";
 import "./Home.css";
+import apitmdb, { API_PARAMS, INTERNAL_API_PARAMS } from "../../services/api-tmdb";
 
 export default function Home() {
+    
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function loadMoviesFromApi() {
+            const endpointUrl = API_PARAMS.endpoints.nowplaying;
+            
+            console.log(endpointUrl);
+
+            const response = await apitmdb.get(endpointUrl,
+                { params: {
+                    api_key: INTERNAL_API_PARAMS.key,
+                    language: INTERNAL_API_PARAMS.lang,
+                    page: INTERNAL_API_PARAMS.page
+                }}
+            );
+            
+            console.log(response);
+            return response;
+        }
+
+        setMovies(loadMoviesFromApi());
+        console.log(movies);
+    }, []);
+
     return(
     <main>
         <p>Home</p>
