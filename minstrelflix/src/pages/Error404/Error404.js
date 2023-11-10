3a821550acb410822744a98fe01589d71cd755a5
Michael Kevin <michaelkmb@hotmail.com>
2023-11-10 19:30:41 -0300

    misc Error404 structure;

--- minstrelflix/src/pages/Error404/Error404.js
+++ minstrelflix/src/pages/Error404/Error404.js
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
 import "./Error404.css";
 
+import Home from "../Home/Home";
+
 export default function Error404() {
-    return(<main>
-        <p>Error404</p>
+    const error404Url = "https://dev-to-uploads.s3.amazonaws.com/i/7aqcppklh6bexoa70320.jpg";
+    return(<main className={'main__sect main'}>
+        <article className={"error__artc"}>
+            <section className={"error-pic__sect"}>
+                <p className={"e404__pic"}></p>
+            </section>
+            <section className={"error-txt__sect"}>
+                <h2 className={"error__title"}>Error404</h2>
+                <p className={"error__txt"}>Desculpe, mas não entendi onde você queria chegar.</p>
+                <p className={"not-found__txt"}>
+                    <strong>Página não encontrada</strong>
+                    <Link to="/" element={ <Home />}>Voltar para a página inicial</Link>
+                </p>
+            </section>
+        </article>
     </main>);
 }
