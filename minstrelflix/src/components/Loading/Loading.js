015029f5a75d9242c9da1bc95c642c187a42f442
Michael Kevin <michaelkmb@hotmail.com>
2023-11-10 21:10:46 -0300

    feat Loading Component;

---
+++ minstrelflix/src/components/Loading/Loading.js
@@ -0,0 +1,10 @@
+import "./Loading.css";
+
+function Loading() {
+    return(
+    <main className={"loading"}>
+        <h2>Carregando, por favor aguarde.</h2>
+    </main>);
+}
+
+export default Loading;
