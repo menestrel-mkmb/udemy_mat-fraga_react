391c9806c1183f56d470f504b66bb022a7f766d7
Michael Kevin <michaelkmb@hotmail.com>
2023-11-13 17:49:37 -0300

    misc Movie Page structure;

--- minstrelflix/src/pages/Movie/Movie.js
+++ minstrelflix/src/pages/Movie/Movie.js
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./Movie.css";
 
-import apitmdb, { API_PARAMS, INTERNAL_API_PARAMS } from "../../services/api-tmdb";
 import Loading from "../../components/Loading/Loading";
+
+import apitmdb, { API_PARAMS, INTERNAL_API_PARAMS } from "../../services/api-tmdb";
+import { urlTitle } from "../../services/imdb";
 
 export default function Movie() {
     const { movieId } = useParams();
@@ -49,8 +51,33 @@ export default function Movie() {
                     alt={"Pôster do filme " + movie.title}
                 />
                 <section className={"general-txt__sect"}>
-                    <p>{movie.overview}</p>
-                    <h3>Título original: {movie.original_title}</h3>
+                    <p className={"overview__txt"} >{movie.overview.length > 0 ? movie.overview : "Não foi encontrada descrição."}</p>
+                    <p className={"field__txt"}>Título original: 
+                        <strong className={"info__txt"}> {movie.original_title}</strong>
+                    </p>
+                    <p className={"field__txt"}>Gênero(s): 
+                        <strong className={"info__txt"}>{movie.genres.map(
+                            (genre, index, arr) => `${genre.name}${index < arr.length-1 ? ", " : "."}`)}
+                        </strong>
+                    </p>
+                    <p className={"field__txt"}>Data de lançamento:
+                        <strong className={"info__txt"}>{movie.release_date}</strong>
+                    </p>
+                    <p className={"field__txt"}>Nota do público: 
+                        <strong className={"info__txt"}>{movie.vote_average}/10 ({movie.vote_count})</strong>
+                    </p>
+                    <p className={"field__txt links__sect"}>Links  
+                        <strong className={"info__txt"}>
+                            <a
+                                href={urlTitle + movie.imdb_id}
+                                target={"_blank"} rel="noreferrer"
+                            >
+                                Página no IMDB
+                            </a>
+                        </strong>
+                        
+                    </p>
+
                 </section>
             </section>
         </article>
---
+++ minstrelflix/src/services/imdb.js
@@ -0,0 +1,1 @@
+export const urlTitle = "https://www.imdb.com/title/";
