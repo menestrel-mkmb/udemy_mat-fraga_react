4efd038c803bb432d9a9ddd6a5ec11ced8ea8fd5
Michael Kevin <michaelkmb@hotmail.com>
2023-11-14 23:06:25 -0300

    fix text section Card Component;

--- minstrelflix/src/components/Card/Card.js
+++ minstrelflix/src/components/Card/Card.js
@@ -6,16 +6,18 @@ import { API_PARAMS } from "../../services/api-tmdb";
 function Card(props) {
     const movie = props.movie;
     const index = props.index;
-
+    const lang = props.lang;
 
     return(
     <section className={`movie__sect movie movie-${index} card`}>
         <Link to={`/movie/${movie.id}`}>
-            <h2>{movie.title}</h2>
-            <h3>IMDB ID: {movie.id}</h3>
             <img className={"movie__img"}
                 src={ API_PARAMS.image_base_url + API_PARAMS.image_w300_size + movie.poster_path}
             />
+            <section className={"movie-text__sect"}>
+                <h2 className={"movie__title"}>{movie.title}</h2>
+                { !(lang === 'pt') &&<h3 className={"original__title"}>{movie.original_title}</h3>}
+            </section>
         </Link>
     </section>);
 }
