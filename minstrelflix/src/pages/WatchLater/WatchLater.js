b0aa9c97bcf633355540b2aafaf4908e1493c5e4
Michael Kevin <michaelkmb@hotmail.com>
2023-11-20 17:20:17 -0300

    misc Add Favorite n Delete Movie from list;

--- minstrelflix/src/pages/WatchLater/WatchLater.js
+++ minstrelflix/src/pages/WatchLater/WatchLater.js
@@ -11,18 +11,49 @@ function WatchLater() {
         setMovies(parsedList);
     }, []);
 
+    const saveList = (string, obj) => {
+        localStorage.setItem(string, JSON.stringify(obj));
+    }
+
+    const addFavorite = (index) => {
+        alert(`fav ${index}`);
+    }
+
+    const deleteMovie = (index) => {
+        const tempMovieList = movies.filter(
+            (movie, movieIndex) => movieIndex !== index
+        )
+        setMovies(tempMovieList);
+        saveList("@toWatchList", tempMovieList);
+
+    }
+
     return(
     <main className={"main__sect main"}>
-        <h2>Assistir mais tarde</h2>
+        <h2 className={"watch-later__title"}>Assistir mais tarde</h2>
         <article className={"watch-later__artc movies__artc movies container"}>
             {movies.length === 0 ? (<p>"Não foi encontrado nenhum filme salvo."</p>) :
                 movies.map(
                 (movie, index) => {
                     return(
-                        <Card key={movie.id}
-                            index={index} movie={movie}
-                            lang={movie.original_lang}
-                        />
+                        <section key={movie.id}
+                            className={`movie__sect movie-${index}`}
+                        >
+                            <button className={`obj__btn favorite__btn`}
+                                onClick={ () => {addFavorite(index)}}
+                            >
+                                F
+                            </button>
+                            <button className={`obj__btn delete__btn`}
+                                onClick={ () => {deleteMovie(index)}}
+                            >
+                                X
+                            </button>
+                            <Card
+                                index={index} movie={movie}
+                                lang={movie.original_lang}
+                            />
+                        </section>
                     )
                 }
             )}
