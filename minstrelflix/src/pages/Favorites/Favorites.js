56161c1a19fa676752b33721ed0575991ca35331
Michael Kevin <michaelkmb@hotmail.com>
2023-11-20 16:44:58 -0300

    feat Favorites page shows movies Card;

--- minstrelflix/src/pages/Favorites/Favorites.js
+++ minstrelflix/src/pages/Favorites/Favorites.js
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
+import Card from "../../components/Card/Card";
+
 import "./Favorites.css";
 
 export default function Favorites() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        const favList = localStorage.getItem("@favoriteList");
+        const parsedList = JSON.parse(favList);
+        setMovies(parsedList);
+    }, []);
+
     return(
-    <main>
-        <p>Favorites</p>
+    <main className={"main__sect main"}>
+        <h2 className="favorites__title">Favoritos</h2>
+        <article className={"favorites__artc movies__artc movies container"}>
+            {movies.length === 0 ? (<p>"Não foi encontrado nenhum filme salvo."</p>) :
+                movies.map(
+                (movie, index) => {
+                    return(
+                        <Card key={movie.id}
+                            index={index} movie={movie}
+                            lang={movie.original_lang}
+                        />
+                    )
+                }
+            )}
+        </article>
     </main>);
 }
