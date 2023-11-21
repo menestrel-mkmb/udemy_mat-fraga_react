951188132a0ce9eb46721b4d570d28489aa8ca66
Michael Kevin <michaelkmb@hotmail.com>
2023-11-21 16:38:39 -0300

    style Loading; misc style Card from Page;

--- minstrelflix/src/components/Loading/Loading.js
+++ minstrelflix/src/components/Loading/Loading.js
@@ -2,8 +2,13 @@ import "./Loading.css";
 
 function Loading() {
     return(
-    <main className={"loading"}>
-        <h2>Carregando, por favor aguarde.</h2>
+    <main className={"loading__sect main__sect main"}>
+        <h2 className={"loading__title"}>Carregando, por favor aguarde</h2>
+        <section className={"dots__sect"}>
+            <span className={"dot dot-1"}></span>
+            <span className={"dot dot-2"}></span>
+            <span className={"dot dot-3"}></span>
+        </section>
     </main>);
 }
 
