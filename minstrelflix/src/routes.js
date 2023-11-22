885da6de4e347ada86d15b392782436a3d70643f
Michael Kevin <michaelkmb@hotmail.com>
2023-11-22 17:21:02 -0300

    feat Footer Component;

--- minstrelflix/src/routes.js
+++ minstrelflix/src/routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from "./components/Header/Header";
+import Footer from './components/Footer/Footer';
 
 import Home from "./pages/Home/Home";
 import Error404 from "./pages/Error404/Error404";
@@ -19,6 +20,7 @@ function RoutesApp() {
 
             <Route path="/*" element={ <Error404 /> } />
         </Routes>
+        <Footer />
     </BrowserRouter>);
 }
 
