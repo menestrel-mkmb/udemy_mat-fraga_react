1ca8989fb12c4087e26122249da54abf1d62d438
Michael Kevin <michaelkmb@hotmail.com>
2023-12-07 10:14:31 -0300

    misc package firebase n firestore db;

--- firebase-app/src/App.js
+++ firebase-app/src/App.js
@@ -1,3 +1,5 @@
+import { firebasedb } from "./services/firebase/firebaseconnection";
+
 function App() {
   return (
     <div className="App">
---
+++ firebase-app/src/services/firebase/firebaseconnection.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAIX9oDIt0Ck61gDugY5YLcaGxl0iixoIg',
+  authDomain: 'movieteller-8fa40.firebaseapp.com',
+  projectId: 'movieteller-8fa40',
+  storageBucket: 'movieteller-8fa40.appspot.com',
+  messagingSenderId: '504280616253',
+  appId: '1:504280616253:web:ab3fe8dfda1e90ec4e0954'
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+export const firebasedb = getFirestore(firebaseApp);
